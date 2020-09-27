dcbb21bb8fc18a9083aba56798f5c2038992cccf
JohnFury77 <furijani99@gmail.com>
2020-09-27 19:21:00 +0200

    2. feladat

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -10,7 +10,22 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
-             
+            double atlag = 0;
+            int[] szamok = new int[10];
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine("Kérem a(z) " + i + ". számot!");
+                szamok[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                atlag += szamok[i];
+            }
+            atlag = atlag / szamok.Length;
+            Console.WriteLine("A " + szamok.Length + " darab számnak " + atlag + " az átlaga.");
+            Console.ReadKey();
+
         }
     }
 }
